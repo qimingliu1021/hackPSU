f66146b8587a65abfc218af4e40bd571682720f9
harshim1 <65602657+harshim1@users.noreply.github.com>
2024-03-17 12:44:07 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -40,6 +40,20 @@ def plot_data_by_column_g(data_file, column_g, column_d, column_e):
    plt.show()
 
 @app.route('/test')
+filtered_data = data[data["Column D"] > 2018]                                   
+def search_data():
+    search_term = search_var.get().lower()
+    matching_data = filtered_data[
+        (filtered_data["Column B"].str.lower().contains(search_term))
+        | (filtered_data["Column C"].str.lower().contains(search_term))
+    ]
+
+    if matching_data.empty:
+        result_label.config(text="No matching results found.")
+    else:
+        result_label.config(text=f"Value (Column G): {matching_data.iloc[0]['Column G']}")
+        plot_data_by_column_g("dataset.xlsx",Column G, Column C)
+
 
 @app.route('/test')
 def classify_air_quality(model, features, scaler=None):
